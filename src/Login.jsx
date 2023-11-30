9412c2ed1ab821a513962720c5cb063fc87bee34
Elhoucine <houcineparade@gmail.com>
2023-11-30 12:26:53 +0100

    feat: add login notifications, and handle failure

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,26 +1,41 @@
 import { useState, useEffect, useRef } from 'react';
 import { Helmet } from 'react-helmet-async';
+import { ToastContainer, toast } from 'react-toastify';
 import Webcam from "react-webcam";
+import 'react-toastify/dist/ReactToastify.css';
 
 const CDN_BASE_URL = 'https://cdnjs.cloudflare.com/ajax/libs';
 const TACKINGJS_CDN = `${CDN_BASE_URL}/tracking.js/1.1.3/tracking-min.js`;
 const FACEJS_CDN = `${CDN_BASE_URL}/tracking.js/1.1.3/data/face-min.js`;
+const LOGIN_DELAY_SECONDS = 10;
+const SUCCESS_MSG = 'You are being redirected to hello page!';
+const ERROR_MSG = 'You are about to login biometrically.';
 
 function Login({ onLogin }) {
+  let tracker;
   const webcamRef = useRef(null);
+  const [isFaceCaptured, setIsFaceCaptured] = useState(false);
   const [isCameraOn, setIsCameraOn] = useState(true);
 
   useEffect(() => {
+    // using a node_module would have been easier,
+    // for the sake of this challenge I am loading this external script instead.
+    let scriptsLoaded = 0;
     const script1 = document.createElement('script');
     const script2 = document.createElement('script');
     script1.src = TACKINGJS_CDN;
     script2.src = FACEJS_CDN;
 
-    script1.onload = () => {
-      script2.onload = () => {
+    // Make sure the two scripts are loaded. 
+    const initTracking = () => {
+      scriptsLoaded++;
+      if (scriptsLoaded === 2) {
         trackFace();
-      };
+      }
     }
+
+    script1.onload = initTracking
+    script2.onload = initTracking;
 
     document.head.appendChild(script1);
     document.head.appendChild(script2);
@@ -31,34 +46,56 @@ function Login({ onLogin }) {
     };
   }, []);
 
+  const handleOnRetry = () => {
+    document.location.reload();
+  }
+
   const trackFace = () => {
-    const tracker = new tracking.ObjectTracker('face');
+    tracker = new tracking.ObjectTracker('face');
     tracker.setInitialScale(4);
     tracker.setStepSize(2);
     tracker.setEdgesDensity(0.1);
 
     tracker.on('track', function(event) {
-      if (event.data.length === 0) {
-        console.log('No face');
-      } else {
-        console.log('Face detected');
-        setIsCameraOn(false);
-        onLogin(true);
+      if (event.data.length > 0 && !isFaceCaptured) {
+        setIsFaceCaptured(true);
         tracker.removeAllListeners();
+        toast.success(SUCCESS_MSG);
+        // allow notification sometime before redirecting.
+        setTimeout(() => {
+          setIsCameraOn(false);
+          onLogin(true);
+        }, 1000 * 3)
       }
     });
 
     tracking.track(webcamRef.current.video, tracker, { camera: true });
+    tryLoginFor();
+  }
+
+  const tryLoginFor = () => {
+    setTimeout(() => {
+      // ignore failure if user is logging-in.
+      if (!isFaceCaptured) {
+        tracker?.removeAllListeners?.();
+        setIsCameraOn(false);
+        toast.error("Login failed!");
+      }
+    }, LOGIN_DELAY_SECONDS * 1000)
   }
 
   return (
     <>
       <Helmet>
         <title>Login page!</title>
-        <meta name='description' content='You are about to login biometrically.' />
+        <meta name='description' content={ERROR_MSG} />
       </Helmet>
       <h1>CAMERA</h1>
-      {isCameraOn && <Webcam ref={webcamRef}/>}
+      {isCameraOn
+        ? <Webcam ref={webcamRef}/>
+        : <button onClick={handleOnRetry}>Re-try</button>
+      }
+      <ToastContainer />
     </>
   )
 }
